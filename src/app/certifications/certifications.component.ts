17bec024e9e96b775cc9caf41ca73f966b030712
Shreyansh Shikhar Srivastava <133575578+shreyanshshikhar@users.noreply.github.com>
2024-01-20 18:07:09 +0530

    Update certifications.component.ts

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./certifications.component.css']
 })
 export class CertificationsComponent {
+  certificates = [
+    { title: 'Node js (Basics)', imageUrl: 'assets/node.png', link: 'https://www.hackerrank.com/certificates/00f21150a082' },
+    { title: 'Javascript (Basics)', imageUrl: 'assets/js.png', link: 'https://www.hackerrank.com/certificates/1968551051b0' },
+    { title: 'Angular (Basics)', imageUrl: 'assets/angular.png', link: 'https://www.hackerrank.com/certificates/e2d67f7b7d1d' },    { title: 'Css (Basics)', imageUrl: 'assets/css.png', link: 'https://www.hackerrank.com/certificates/a33f4bbf3046' }
 
+  ];
 }
