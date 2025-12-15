a6b3fd6c986e15114587d81be2fd42170cb15291
Shreyansh Shikhar Srivastava <133575578+shreyanshshikhar@users.noreply.github.com>
2025-12-15 15:32:46 +0530

    Update certifications.component.ts

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -9,7 +9,8 @@ export class CertificationsComponent {
   certificates = [
     { title: 'Node js (Basics)', imageUrl: 'assets/node.png', link: 'https://www.hackerrank.com/certificates/00f21150a082' },
     { title: 'Javascript (Basics)', imageUrl: 'assets/js.png', link: 'https://www.hackerrank.com/certificates/1968551051b0' },
-    { title: 'Angular (Basics)', imageUrl: 'assets/angular.png', link: 'https://www.hackerrank.com/certificates/e2d67f7b7d1d' },    { title: 'Css (Basics)', imageUrl: 'assets/css.png', link: 'https://www.hackerrank.com/certificates/a33f4bbf3046' }
+    { title: 'Angular (Basics)', imageUrl: 'assets/angular.png', link: 'https://www.hackerrank.com/certificates/e2d67f7b7d1d' },    { title: 'Css (Basics)', imageUrl: 'assets/css.png', link: 'https://www.hackerrank.com/certificates/a33f4bbf3046' },    { title: 'Google Cloud Computing foundation and Generative Ai (Basics)', imageUrl: 'assets/cloud.png',  }
+
 
   ];
 }
